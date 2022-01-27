b999669983488d31a0ff7dad6c2177da4ee78709
manspitub <74324580+manspitub@users.noreply.github.com>
2022-01-27 14:25:49 +0100

    Casi terminado

--- movies/lib/main.dart
+++ movies/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movies/pages/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -37,35 +38,9 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      backgroundColor: Colors.white,
-
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: new Text("Erik Howell", style: TextStyle(color: Colors.black),),
-    leading: new Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: new Material(
-        color: Colors.grey[300],
-        child: Image.network("https://todonexus.com/wp-content/uploads/Que-significa-la-mujer-sin-rostro-de-WhatsApp-300x300.png", ),
-        shape: new CircleBorder(),
-
-    ),
-  
-    ),
-    
-    actions: [
-
-      Container(
-        margin: EdgeInsets.only(right: 15),
-        child: CircleAvatar(
-          backgroundImage: Image.network("https://elamigoinformaticoblog.files.wordpress.com/2019/01/cast-icon-chromecast.jpg").image,
-          foregroundColor: Colors.grey,
-
-        ) 
-      )
-    ],
-    )
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
--- movies/lib/services/api_service.dart
+++ movies/lib/services/api_service.dart
@@ -1,22 +1,14 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:movies/models/upcoming.dart';
+import 'package:http/http.dart' as http;
+
 
 class ApiService {
   final Dio _dio = Dio();
 
-  final String baseUrl = 'https://api.themovie.org/3';
-  final String apiKey = '6f3f957d9d8dc6a98fed68a333b8209c';
-
-  Future<List<Upcoming>> getUpcomingMovies() async {
-    try{
-      final response await _dio.get('$baseUrl/get-upcoming?$apiKey');
-      
-    }catch(error, stackTrace){
-      throw Exception(
-        'Excepcion'
-      );
-    }
-  }
+  
 
 
 }
