9e6cd65835c637e8cf427ed011c37f17207f456c
manspitub <manspitub@hotmail.com>
2022-03-21 13:47:56 +0100

    Lista de compañias terminada

--- gasolinerapp/lib/data/mock.dart
+++ gasolinerapp/lib/data/mock.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class LuzModel {
   LuzModel({required this.name, required this.price, required this.logo, required this.phone, required this.workTime});
   String name;
@@ -8,7 +10,7 @@ class LuzModel {
 }
 
 final companiasLuz = [
-  LuzModel(name: 'Naturgy', price: 0.24, logo: 'naturgy.svg', phone: '900100251', workTime: 'Llámanos de lunes a sábado - de 8:00h a 22:00h'),
-  LuzModel(name: 'Endesa', price: 0.24, logo: 'naturgy.svg', phone: '900100251', workTime: 'Llámanos de lunes a sábado - de 8:00h a 22:00h'),
-  LuzModel(name: 'Iberdrola', price: 0.24, logo: 'naturgy.svg', phone: '900100251', workTime: 'Llámanos de lunes a sábado - de 8:00h a 22:00h'),
+  LuzModel(name: 'Naturgy', price: 0.24, logo: 'naturgy.png', phone: '900100251', workTime: 'Llámanos de lunes a sábado - de 8:00h a 22:00h'),
+  LuzModel(name: 'Endesa', price: 0.24, logo: 'naturgy.png', phone: '900100251', workTime: 'Llámanos de lunes a sábado - de 8:00h a 22:00h'),
+  LuzModel(name: 'Iberdrola', price: 0.24, logo: 'naturgy.png', phone: '900100251', workTime: 'Llámanos de lunes a sábado - de 8:00h a 22:00h'),
 ];
--- gasolinerapp/lib/screens/luz_screen.dart
+++ gasolinerapp/lib/screens/luz_screen.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:gasolinerapp/data/mock.dart';
 
 class LuzScreen extends StatefulWidget {
-  const LuzScreen({ Key? key }) : super(key: key);
+  const LuzScreen({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<LuzScreen> createState() => _LuzScreenState();
 }
 
 class _LuzScreenState extends State<LuzScreen> {
+
+  
+
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,9 +25,127 @@ class _LuzScreenState extends State<LuzScreen> {
           children: [
             Row(
               children: [
-                Text("Compañías Eléctricas", style: TextStyle(fontSize: 20, color: Colors.blue[600], letterSpacing: 2),),
-                Icon(Icons.arrow_circle_down, size: 40,)
+                Text(
+                  "Compañías Eléctricas",
+                  style: TextStyle(
+                      fontSize: 20, color: Colors.blue[600], letterSpacing: 2),
+                ),
+                Icon(
+                  Icons.arrow_circle_down,
+                  size: 40,
+                )
               ],
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: GridView.builder(
+                itemCount: companiasLuz.length,
+                itemBuilder: ((context, index) {
+                  return SizedBox(
+                    height: MediaQuery.of(context).size.height,
+                    child: Card(
+                      child: InkWell(
+                        splashColor: Colors.blue.withAlpha(30),
+                        onTap: () {
+                          debugPrint('Card tapped.');
+                        },
+                        child: SizedBox(
+                            width: 100,
+                            height: 50,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.all(15.0),
+                                      child: Text(companiasLuz[index].name,
+                                          style: TextStyle(
+                                              fontSize: 18,
+                                              color: Colors.blue.shade800,
+                                              fontWeight: FontWeight.bold)),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.all(15.0),
+                                      child: Image.asset(
+                                        companiasLuz[index].logo, width: 70,
+
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.all(15.0),
+                                      child: Container(
+                                        child: ElevatedButton.icon(
+                                            onPressed: () {},
+                                            icon: Icon(
+                                              Icons.arrow_drop_down,
+                                              size: 40.0,
+                                              color: Colors.red,
+                                            ),
+                                            label: Text(
+                                              companiasLuz[index].price.toString(),
+                                              style: TextStyle(
+                                                  fontSize: 25,
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.bold),
+                                            )),
+                                        width: 150,
+                                        height: 40,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Column(
+                                      children: [
+                                        ElevatedButton.icon(
+                                            style: ButtonStyle(
+                                              backgroundColor: MaterialStateProperty.all(Colors.red)
+                                            ),
+                                            onPressed: () {},
+                                            icon: Icon(
+                                              Icons.phone,
+                                              size: 10,
+                                              color: Colors.blue,
+                                            ),
+                                            label: Text(
+                                              companiasLuz[index].toString(),
+                                              style: TextStyle(
+                                                  fontSize: 10,
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.bold),
+                                            )),
+                                            
+                                        Text(
+                                          companiasLuz[index].workTime,
+                                          style: TextStyle(
+                                              fontSize: 10,
+                                              fontWeight: FontWeight.bold),
+                                        )
+                                      ],
+                                    )
+                                  ],
+                                )
+                              ],
+                            )),
+                      ),
+                    ),
+                  );
+                }),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                ),
+              ),
             )
           ],
         ),
